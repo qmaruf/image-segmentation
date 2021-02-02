69948fc03d1d429916df9518323e6b1a01aaa96c
Quazi Marufur Rahman <maruf.csdu@gmail.com>
2021-02-03 00:41:00 +1000

    working good

--- dataset.py
+++ dataset.py
@@ -15,24 +15,18 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 
-
 class SegmentationDataset(Dataset):
 	def __init__(self, images, masks, training):
 		self.images = images
 		self.masks = masks  
 		self.training = training
-		# self.transforms_img = tvt.Compose([        
-		#     tvt.ToTensor(),
-		#     tvt.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])   
-		# self.transforms_lbl = tvt.Compose([        
-		#     tvt.ToTensor()])   
 		
 		
-		self.semantic_aug = A.Compose([    
-			A.HorizontalFlip(p=0.5),		
+		self.semantic_aug_train = A.Compose([    
+            A.CenterCrop(height=config.IMG_HEIGHT//2, width=config.IMG_WIDTH//2),
+            A.HorizontalFlip(p=0.5),		
 		])
-		self.train_aug = A.Compose([
-			A.HorizontalFlip(p=0.5),
+		self.train_aug = A.Compose([			            
 			A.OneOf([
 				A.IAAAdditiveGaussianNoise(),
 				A.GaussNoise(),
@@ -46,9 +40,11 @@ class SegmentationDataset(Dataset):
 				A.HueSaturationValue()
 			]),
 			A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))        
+		])		
+		self.semantic_aug_eval = A.Compose([    
+            A.CenterCrop(height=config.IMG_HEIGHT//2, width=config.IMG_WIDTH//2)
 		])
-		
-		self.eval_aug = A.Compose([
+		self.eval_aug = A.Compose([			
 			A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))        
 		])
 		
@@ -67,10 +63,12 @@ class SegmentationDataset(Dataset):
 		sample['original_image'] = self.to_tensor(image=image)['image']
 	
 		if self.training:
-			augmented = self.semantic_aug(image=image, mask=mask)
+			augmented = self.semantic_aug_train(image=image, mask=mask)
 			image, mask = augmented['image'], augmented['mask']
 			image = self.train_aug(image=image)['image']    	
 		else:
+			augmented = self.semantic_aug_eval(image=image, mask=mask)
+			image, mask = augmented['image'], augmented['mask']
 			image = self.eval_aug(image=image)['image']    
 		
 		image = self.to_tensor(image=image)['image']			
@@ -88,7 +86,7 @@ def create_data_loader(images, masks, batch_size, training):
 def read_data():    
 	masks = glob(config.LABELS_DIR + '*.png')
 	if config.SAMPLE:
-		masks = masks[:10]
+		masks = masks[:64]
 	images = [config.IMGS_DIR + Path(mask).stem.split('_')[0] + '.jpg' for mask in masks]
 	images_train, images_test, masks_train, masks_test = train_test_split(images, masks, test_size=0.1,random_state=config.RANDOM_SEED)
 	images_train, images_val, masks_train, masks_val = train_test_split(images_train, masks_train, test_size=0.2,random_state=config.RANDOM_SEED)
--- models.py
+++ models.py
@@ -12,120 +12,125 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 class SemanticSegmentation(nn.Module):
-  def __init__(self, n_classes):
-    super(SemanticSegmentation, self).__init__()
-    self.model = model = smp.DeepLabV3(
-        encoder_name=config.SMP_ENCODER,        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-        encoder_weights="imagenet",     # use `imagenet` pretrained weights for encoder initialization
-        in_channels=3,                  # model input channels (1 for grayscale images, 3 for RGB, etc.)
-        classes=2, 
-    )   
+	def __init__(self, n_classes):
+		super(SemanticSegmentation, self).__init__()
+		self.model = model = smp.DeepLabV3(
+			encoder_name=config.SMP_ENCODER,        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+			encoder_weights="imagenet",     # use `imagenet` pretrained weights for encoder initialization
+			in_channels=3,                  # model input channels (1 for grayscale images, 3 for RGB, etc.)
+			classes=2, 
+		)
+		self.conv1 = nn.Conv2d(2, 4, 3, stride=2)   
+		self.conv2 = nn.Conv2d(4, 8, 3, stride=2)   
+		self.conv3 = nn.Conv2d(8, 16, 3, stride=2)   
+		self.conv4 = nn.Conv2d(16, 32, 3, stride=2)   
+		self.pool = nn.MaxPool2d(2, 2)
+		self.fc = nn.Linear(16*10*19, 1)
+		self.relu = nn.ReLU()
+		self.loss_net = self.get_loss_net()
+		self.mse_loss = nn.MSELoss()
 
-  def forward(self, input_):
-    prediction = self.model(input_)
-    return prediction
+	def get_loss_net(self):
+		layers = []
+		for inc, outc in [[2, 4], [4, 8], [8, 16]]:
+			layers.extend([
+				nn.Conv2d(inc, outc, 3, stride=2),
+				nn.ReLU(),
+				nn.MaxPool2d(2, 2)])
+		return nn.Sequential(*layers)
+
+	def forward(self, input_):
+		x1 = self.model(input_)		
+		return x1, None
 
 
 def tensor_to_img(tnsr, output):
-    img = tvt.ToPILImage()(tnsr)
-    img.save(output)
-    # exit()
-    
+	img = tvt.ToPILImage()(tnsr)
+	img.save(output)
+	# exit()
+	
 class SemanticSegmentationModel(pl.LightningModule):
-    def __init__(self, train_dl=None, val_dl=None, test_dl=None):
-        super(SemanticSegmentationModel, self).__init__()        
-        self.model = SemanticSegmentation(n_classes=config.N_CLASS)
-        self.diceloss = smp.losses.DiceLoss(mode='binary')         
-        self.learning_rate = 0.0001     
-        self.train_dl, self.val_dl, self.test_dl = train_dl, val_dl, test_dl
-        self.sigmoid = nn.Sigmoid()
-        self.mseloss = nn.MSELoss()
-        self.cnt = 0
+	def __init__(self, train_dl=None, val_dl=None, test_dl=None):
+		super(SemanticSegmentationModel, self).__init__()        
+		self.model = SemanticSegmentation(n_classes=config.N_CLASS)
+		self.diceloss = smp.losses.DiceLoss(mode='binary')         
+		self.learning_rate = config.LEARNING_RATE   
+		self.train_dl, self.val_dl, self.test_dl = train_dl, val_dl, test_dl
+		self.sigmoid = nn.Sigmoid()
+		self.mseloss = nn.MSELoss()
+		self.cnt = 0
 
-    def criterion(self, mask_pred, mask_true):
-        
-        '''
-        return diceloss * 0.8 + diffloss * 0.2 works good
-        '''
-        
-        pred_segm = mask_pred[:, 0, :, :].unsqueeze(1)
-        pred_fail = mask_pred[:, 1, :, :].unsqueeze(1)
-        
-        diceloss = self.diceloss(pred_segm, mask_true)
-        
-        pred_segm = self.sigmoid(pred_segm)
-        diff = mask_true - pred_segm
-        diff = (diff > 0.5).type(torch.float) # detect high difference area
-                
-        # if self.cnt % 10 == 0:
-        #     tensor_to_img(diff[0], 'diff.jpg')
-        #     tensor_to_img(pred_segm[0], 'prediction.jpg')
-        #     tensor_to_img(mask_true[0], 'label.jpg')
-        
-        self.cnt = (self.cnt + 1)%10
-        
-        diffloss = self.diceloss(pred_fail, diff)
-        
-        # self.log('')
-        # print ('\n diceloss \t %f diffloss \t %f'%(diceloss, diffloss))
-        
-        
-        loss = diceloss * 0.9 + diffloss * 0.1
-        
-        return loss
-        
-        
-        if self.cnt < 5:
-            print ('using diceloss')
-            loss = diceloss
-        else:
-            print ('using diffloss')
-            loss = diffloss
-            
-        return loss
-        
-        
-    def forward(self, input):
-        return self.model(input)
+	def criterion(self, mask_pred, loss_pred, mask_true):
+		'''
+		return diceloss * 0.8 + diffloss * 0.2 works good
+		'''
+		pred_segm = mask_pred[:, 0, :, :].unsqueeze(1)
+		pred_fail = mask_pred[:, 1, :, :].unsqueeze(1)
+		diceloss = self.diceloss(pred_segm, mask_true)
 
-    def training_step(self, batch, batch_nb):
-        inputs, targets = batch['image'], batch['mask']
-        preds = self.forward(inputs)        
-        loss = self.criterion(preds, targets)
-        tensorboard_logs = {'train_loss': loss}
-        return {'loss': loss, 'log': tensorboard_logs}
+		pred_segm = self.sigmoid(pred_segm)
+		pred_segm = (pred_segm > 0.5).type(torch.float)
+  
+		diff = torch.abs(mask_true - pred_segm)
+		# diff = (diff > 0.5).type(torch.float) # detect high difference area
+		diffloss = self.diceloss(pred_fail, diff)
+		
+		loss = diceloss * 0.8 + diffloss * 0.2
+		
+		loss_dict = {
+			'diceloss': diceloss,
+			'diffloss': diffloss			
+		}
+		
+		return loss, loss_dict
+		
+	def forward(self, input):
+		return self.model(input)
 
-    def validation_step(self, batch, batch_nb):
-        inputs, targets = batch['image'], batch['mask']
-        preds = self.forward(inputs)                
-        loss = self.criterion(preds, targets)
-        return {'val_loss': self.criterion(preds, targets)}
+	def training_step(self, batch, batch_nb):
+		inputs, targets = batch['image'], batch['mask']
+		seg_preds, loss_preds = self.forward(inputs)        
+		loss, loss_dict = self.criterion(seg_preds, loss_preds, targets)
+		tensorboard_logs = {'train_loss': loss}
+		tensorboard_logs.update(loss_dict)
+		return {'loss': loss, 'train_loss': loss, 'log': tensorboard_logs}
 
-    def validation_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        tensorboard_logs = {'val_loss': avg_loss}
-        return {'avg_val_loss': avg_loss, 'log': tensorboard_logs}
+	def validation_step(self, batch, batch_nb):
+		inputs, targets = batch['image'], batch['mask']
+		# print (inputs.size(), targets.size())
+		# exit()
+		seg_preds, loss_preds = self.forward(inputs)                
+		loss, loss_dict = self.criterion(seg_preds, loss_preds, targets)
+		tensorboard_logs = {'train_loss': loss}
+		tensorboard_logs.update(loss_dict)
+		return {'loss': loss, 'val_loss': loss, 'log': tensorboard_logs}
 
-    def test_step(self, batch, batch_nb):
-        inputs, targets = batch['image'], batch['mask']
-        preds = self.forward(inputs)                
-        return {'test_loss': self.criterion(preds, targets)}
+	def validation_end(self, outputs):
+		avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+		tensorboard_logs = {'val_loss': avg_loss}
+		return {'avg_val_loss': avg_loss, 'log': tensorboard_logs}
 
-    def test_end(self, outputs):
-        avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
-        logs = {'test_loss': avg_loss}
-        return {'avg_test_loss': avg_loss, 'log': logs, 'progress_bar': logs}
+	def test_step(self, batch, batch_nb):
+		inputs, targets = batch['image'], batch['mask']
+		seg_preds, loss_preds = self.forward(inputs)                
+		loss, loss_dict = self.criterion(seg_preds, loss_preds, targets)
+		return {'test_loss': loss}
 
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3, verbose=True)
-        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
+	def test_end(self, outputs):
+		avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
+		logs = {'test_loss': avg_loss}
+		return {'avg_test_loss': avg_loss, 'log': logs, 'progress_bar': logs}
 
-    def train_dataloader(self):
-        return self.train_dl
+	def configure_optimizers(self):
+		optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+		scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=1, verbose=True)
+		return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
 
-    def val_dataloader(self):
-        return self.val_dl
+	def train_dataloader(self):
+		return self.train_dl
 
-    def test_dataloader(self):
-        return self.test_dl
+	def val_dataloader(self):
+		return self.val_dl
+
+	def test_dataloader(self):
+		return self.test_dl
